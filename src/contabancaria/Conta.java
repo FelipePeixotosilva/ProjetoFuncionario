2806793ff262c458ae4372b181b046bc561b8e6b
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2018-07-26 22:55:26 -0300

    Update Conta.java

--- src/contabancaria/Conta.java
+++ src/contabancaria/Conta.java
@@ -3,26 +3,51 @@ package contabancaria;
 
 
 public class Conta {
-  int  numero;
- double saldo;
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+private  int  numero;
+private double saldo;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getLimite() {
+        return saldo+limite;
+    }
   double  limite ; 
   String nome;
   
-  public boolean saca(){
-      return true;
+  public boolean saca(double valor){
+      if ( valor <= saldo){
+      saldo = saldo - valor;
+          return true;
+      }
+      else
+          return false;// nao consegui realizar saque
   }
   
-  public double deposita(double valor){
-  return valor;
+  public void deposita(double valor){
+  saldo = saldo+valor;
   }
   
   public void transfere(Conta destino, double  valor){
-  destino.saldo=destino.saldo+valor;
+      if( valor<= saldo){
+      this.saldo=this.saldo - valor;
+      destino.deposita(valor);
+      }
+      else
+          System.out.print("Não foi possivel realizar a transferencia!");
   }
   
-          
-  
-      
-    
+
+
+
 
 }
